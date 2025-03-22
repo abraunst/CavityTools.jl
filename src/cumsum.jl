00ebe3367d71f3b82ba0cc3d5bbf643f5b1f11ce
Alfredo Braunstein <alfredo.braunstein@polito.it>
2025-03-22 11:17:55 +0100

    some small optimizations

--- src/cumsum.jl
+++ src/cumsum.jl
@@ -52,7 +52,7 @@ function Base.getindex(c::CumSum{T,op,init},i) where {T,op,init}
     a = c.acc
     m = init(T)
     K = length(a.sums)
-    for k in K:-1:1
+    @inbounds for k in K:-1:1
         s = a.sums[k]
         if (i >> (k-1)) & 1 == 1
             m = op(m, s[xor(i >> (k-1), 1) + 1])
@@ -65,15 +65,12 @@ function Base.show(io::IO, c::CumSum{T,op,init}) where {T,op,init}
     print(io, "CumSum(", c.acc, op != (+) || init != zero ? "; op=$op, init=$init)" : ")")
 end
 
-Base.:(==)(c::CumSum, v::Vector) = v[1] == c.acc[1] && all(c.acc[i] == v[i] - v[i-1] for i=2:lastindex(v))
-Base.:(==)(v::Vector, c::CumSum) = c == v
-
 function Base.searchsortedfirst(c::CumSum{T,op,init}, r; lt = isless, rev = false) where {T,op,init}
     a = c.acc
-    x = 0
+    x::Int = 0
     m = init(T)
-    for k in length(a.sums):-1:1
-        s = a.sums[k]
+    for k in reverse(eachindex(a.sums))
+        s = @inbounds a.sums[k]
         x + 1 > length(s) && return length(a) + 1
         @inbounds n = op(s[x + 1], m)
         if xor(rev, lt(n, r))
@@ -82,14 +79,18 @@ function Base.searchsortedfirst(c::CumSum{T,op,init}, r; lt = isless, rev = fals
         end
         x <<= 1
     end
-    return (x >>= 1) + 1
+    return (x >> 1) + 1
 end
 
-@inline function Base.iterate(c::CumSum{T, op, init}, state = nothing) where {T, op, init}
+@inline function Base.iterate(c::CumSum{T, op, init}, 
+                state = (fill(init(T), length(c.acc.sums) + 1), 0)) where {T, op, init}
     a = c.acc
-    sums, i = isnothing(state) ? (fill(init(T), length(a.sums) + 1), 0) : state
+    sums, i = state
     i == length(a) && return nothing
-    k = count_ones(xor(i + 1, i))
-    sums[1:k] .= op(sums[k+1], a.sums[k][(i >> (k-1))+1])
+    @inbounds k = count_ones(xor(i + 1, i))
+    @inbounds x = op(sums[k+1], a.sums[k][(i >> (k-1))+1])
+    @inbounds for j in 1:k
+        sums[j] = x
+    end
     first(sums), (sums, i + 1)
 end
